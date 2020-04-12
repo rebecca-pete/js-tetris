cd96b93485d9dd381134dbcd743a2fbd3a20fa56
Rebeccca Peterson <rebecca.peterson.soprano@gmail.com>
2020-04-11 18:01:16 -0700

    Add rotation

--- tetris.js
+++ tetris.js
@@ -9,6 +9,20 @@ const matrix = [
     [0, 1, 0],
 ];
 
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x){
+            if(m[y][x] !== 0 && 
+                (arena[y + o.y] &&
+                arena[y + o.y][x + o.x]) !== 0) {
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
 function createMatrix(w, h) {
     const matrix = [];
     //While h is not zero, decrease h with one
@@ -21,7 +35,7 @@ function createMatrix(w, h) {
 function draw(){
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
-
+    drawMatrix(arena, {x: 0, y: 0});
     drawMatrix(player.matrix, player.pos);
 
 }
@@ -39,9 +53,65 @@ function drawMatrix(matrix, offset){
     });
 }
 
+function merge(arena, player) {
+    player.matrix.forEach((row, y)=> {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
 function playerDrop(){
     player.pos.y++;
+    if (collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
     dropCounter = 0;
+}
+
+function playerMove(dir) {
+    player.pos.x += dir;
+    if (collide(arena, player)) {
+        player.pos.x -= dir;
+    }
+}
+
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while(collide(arena, player)){
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],    
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+    if ( dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
 }
 
 let dropCounter = 0;
@@ -61,7 +131,6 @@ function update(time = 0){
 }
 
 const arena = createMatrix(12, 20);
-// console.log(arena); console.table(arena);
 
 const player = {
     pos: { x: 5, y: 5},
@@ -70,11 +139,20 @@ const player = {
 
 document.addEventListener('keydown', event => {
     if(event.keyCode === 37){
-        player.pos.x--;
+        playerMove(-1);
+        //left
     } else if (event.keyCode === 39) {
-        player.pos.x++;
+        playerMove(1);
+        //right
     } else if (event.keyCode === 40) {
         playerDrop();
+        //q
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+        //w
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
     }
 });
+
 update();
